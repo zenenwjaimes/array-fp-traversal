95dac9da09f60406cf066133899ac9490521b5fb
zenen jaimes <slasherxf@gmail.com>
2020-05-25 20:33:26 -0700

    Fixed unit tests to add example code from ghub.

--- index.unit.test.js
+++ index.unit.test.js
@@ -106,4 +106,23 @@ describe('ArrayStreamT reading', () => {
     expect(arrayStream.tell()).toEqual(2);
     expect(arrayStream.seek(1).tell()).toEqual(3);
   });
+
+  test('ghub example code', () => {
+    const exampleArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    try {
+      const ast = ArrayStreamT.load(exampleArray);
+      const res = ast.read(4); // res = [0, 1, 2, 3]
+      expect(res).toEqual([0, 1, 2, 3]);
+      ast.seek(1); // pointer should be at idx 1
+
+      const res2 = ast.read(1); // [1] and move pointer to idx 2
+      expect(res2).toEqual([1]);
+      ast.seek(2, ArrayStreamT.SeekPos.CURR); // Seeks from current position
+      expect(ast.tell()).toEqual(4);
+      expect(ast.read()).toEqual(5);
+    } catch (err) {
+      // do things here
+    }
+  });
 });
